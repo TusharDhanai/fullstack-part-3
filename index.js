8fb990f50184ef843447afcbc7bb3c78c209e19c
TusharDhanai <dhanai.tushar007@gmail.com>
2023-01-28 20:28:33 +0530

    phonebook backend step 3

--- index.js
+++ index.js
@@ -38,5 +38,16 @@ app.get('/info',(request, response) => {
     `)
 })
 
+app.get('/api/persons/:id',(request, response) => {
+    const id = Number(request.params.id)
+    const person = contacts.find(item => item.id === id)
+    console.log(id,person);
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
 const PORT = 3001
 app.listen(PORT)

afe683a6135066ce24a3edb6690696befbb4ddb3
Tushar Dhanai <84775231+TusharDhanai@users.noreply.github.com>
2023-07-14 12:44:10 +0530

    Print port to console.

--- index.js
+++ index.js
@@ -101,4 +101,6 @@ app.delete('/api/persons/:id',(request, response) => {
 })
 
 const PORT = process.env.PORT || 3001
-app.listen(PORT)
+app.listen(PORT, () => {
+    console.log(`Server running on Port ${PORT}`)
+})
